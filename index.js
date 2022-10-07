c771e55cebdc11c276073c2c4ddca979b7ba41b3
mohRamy <mohammedrdaly@gmail.com>
2022-10-08 09:47:36 +1200

    m1

---
+++ index.js
@@ -0,0 +1,32 @@
+// IMPORTS FROM PACKAGES
+const express = require("express");
+const mongoose = require("mongoose");
+// IMPORTS FROM OTHER FILES
+const authRouter = require("./routers/auth");
+
+// INIT
+const app = express();
+const DB = "mongodb+srv://mohRamy:moha242@mmed9336@cluster0.yrtkhjc.mongodb.net/?retryWrites=true&w=majority";
+
+// middleware
+app.use(express.json());
+app.use(authRouter);
+
+// Connections
+mongoose
+    .connect(DB)
+    .then(() => {
+        console.log("Connection Successful");
+    })
+    .catch((e) => {
+        console.log(e);
+    });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`connected at port ${PORT}`);
+});
+
+module.exports = app;
+
+// https://git.heroku.com/limitless-wildwood-40445.git (origin)
