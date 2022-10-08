e54b9027cf1f6d7fb7a3a2c46c89a0341e37a166
mohRamy <mohammedrdaly@gmail.com>
2022-10-08 23:26:47 +1200

    m

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
-// IMPORTS FROM PACKAGES
+// // IMPORTS FROM PACKAGES
+// const express = require("express");
+// const mongoose = require("mongoose");
+// // IMPORTS FROM OTHER FILES
+// const authRouter = require("./routers/auth");
+
+// // INIT
+// const app = express();
+// const DB = "mongodb+srv://mohammed:moha242@mmed9336@cluster0.zkwhbo4.mongodb.net/?retryWrites=true&w=majority";
+
+// // middleware
+// app.use(express.json());
+// app.use(authRouter);
+
+// // Connections
+// mongoose
+//     .connect(DB)
+//     .then(() => {
+//         console.log("Connection Successful");
+//     })
+//     .catch((e) => {
+//         console.log(e);
+//     });
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//     console.log(`connected at port ${PORT}`);
+// });
+
+// module.exports = app;
+
+// https://git.heroku.com/limitless-wildwood-40445.git (origin)
+
+
 const express = require("express");
-const mongoose = require("mongoose");
-// IMPORTS FROM OTHER FILES
-const authRouter = require("./routers/auth");
-
-// INIT
 const app = express();
-const DB = "mongodb+srv://mohammed:moha242@mmed9336@cluster0.zkwhbo4.mongodb.net/?retryWrites=true&w=majority";
+const mongoose = require('mongoose');
 
-// middleware
-app.use(express.json());
-app.use(authRouter);
+const DB = 'mongodb+srv://mohammedramydaly:moha242mmed9336@cluster0.h2bdlvl.mongodb.net/?retryWrites=true&w=majority';
 
-// Connections
 mongoose
     .connect(DB)
     .then(() => {
@@ -22,11 +47,14 @@ mongoose
         console.log(e);
     });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`connected at port ${PORT}`);
-});
+const port = process.env.PORT || 3000;
+
+app.get('/products', (req, res, next)=>{
+    res.json({'name':'comunter','color':'black'})
+})
+
+app.listen(port, ()=>{
+    console.log("it is working");
+})
 
 module.exports = app;
-
-// https://git.heroku.com/limitless-wildwood-40445.git (origin)
