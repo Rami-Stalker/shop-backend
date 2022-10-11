88fb6b9cd3ce83e4147e514a4dadd2e09db3e564
mohRamy <mohammedrdaly@gmail.com>
2022-10-12 00:26:57 +1200

    ccc

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,18 +1,23 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const auth = async (req, res, next) => {
     try {
-        const token = req.headers('x-auth-token');
-        if (!token) return res.status(401).json({ msg: 'No auth token, access denied' });
+        const token = req.header("x-auth-token");
+        if (!token)
+            return res.status(401).json({ msg: "No auth token, access denied" });
+
         const verified = jwt.verify(token, "passwordKey");
-        if (!verified) return res.status(401).json({msg: 'Token verification faild, authorization denied.'});
+        if (!verified)
+            return res
+                .status(401)
+                .json({ msg: "Token verification failed, authorization denied." });
 
         req.user = verified.id;
         req.token = token;
         next();
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
-}
+};
 
 module.exports = auth;
