d0f3e50f90ee31622cb191673c1eed7408ef5c9c
mohRamy <mohammedrdaly@gmail.com>
2022-10-13 06:48:42 +1200

    d

--- routers/admin.js
+++ routers/admin.js
@@ -1,3 +1,4 @@
+const e = require("express");
 const express = require("express");
 const admin = require("../middlewares/admin");
 const Product = require("../models/product");
@@ -6,20 +7,61 @@ const adminRouter = express.Router();
 // Add Product
 adminRouter.post('/admin/add-product', admin, async (req, res) => {
     try {
-    const { name, description, images, quantity, price, category } = req.body;
-    let product = new Product({
+        const { name, description, images, quantity, price, category } = req.body;
+        let product = new Product({
             name,
             description,
             images,
             quantity,
             price,
             category,
-    });
-    product = await product.save();
-    res.json(product); 
+        });
+        product = await product.save();
+        res.json(product);
     } catch (e) {
         res.status(500).json({ error: e.message });
     }
 });
+
+// get Product 
+
+adminRouter.get('/admin/get-Products', admin, async (req, res) => {
+    try {
+    const products = await Product.find({});
+    res.json(products)
+    } catch (e) {
+        res.status(500).json({ error: e.toString })
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = adminRouter;
