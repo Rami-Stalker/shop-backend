41c135902299460dd1457cf20388ed4ca5d79d62
mohRamy <mohammedrdaly@gmail.com>
2022-11-15 01:36:26 +1200

    df

--- routers/user.js
+++ routers/user.js
@@ -11,12 +11,11 @@ userRouter.post("/api/add-to-cart", async (req, res) => {
     try {
         const { id , ord } = req.body;
         const product = await Product.findById(id);
-        const deleteProduct = await Product.findByIdAndDelete(id);
         
-        if (ord > product.quantity) {
+        if (ord < product.quantity) {
             product.quantity -= ord;
         }else {
-            deleteProduct;
+            await Product.findByIdAndDelete(id);
         }
 
         res.json(product);
