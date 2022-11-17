15956e54e78154affdb70d29d4b600fa13ef7ac0
mohRamy <mohammedrdaly@gmail.com>
2022-11-18 05:51:36 +1200

    ddd

--- routers/user.js
+++ routers/user.js
@@ -99,7 +99,7 @@ userRouter.post("/api/order", auth, async (req, res) => {
         // }
 
         for (let i = 0; i < products.length; i++) {
-            let product = await Product.findById("636bae1a60f7f23ae4263a74");
+            let product = await Product.findById(products[i].id);
 
         for (let j = 0; j < userQuants.length; j++) {
             if (userQuants[j] <= product.quantity) {
